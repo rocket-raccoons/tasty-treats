d389cd74bcfa1cd4050234be35aba889c5f60f57
Murat Çolak <mo939614@gmail.com>
2024-10-09 02:00:34 +0300

    new css

--- src/js/cards.js
+++ src/js/cards.js
@@ -1,21 +1,28 @@
-// API'den fotoğraf çeken ve HTML'deki img'ye ekleyen fonksiyon
+const { preview } = require("vite");
+
 async function fetchRecipePhoto() {
   try {
-    // API'den veriyi çekiyoruz
     const response = await fetch('https://tasty-treats-backend.p.goit.global/api/recipes');
     const data = await response.json();
-
-    // API'nin array döndürdüğünü biliyoruz, ilk öğeyi alıyoruz
-    const photoUrl = data[0].preview; // İlk tarifin 'preview' alanını alıyoruz
-
-    // HTML'deki img etiketini seçip fotoğrafı ekliyoruz
+    const photoUrl = data[3].preview; 
     const cardImg = document.querySelector('.cards-img');
-    cardImg.src = photoUrl; // Fotoğraf URL'sini img etiketine ekliyoruz
-
+    cardImg.src = photoUrl; 
   } catch (error) {
     console.error('API isteği sırasında bir hata oluştu:', error);
   }
 }
+// Sayfa yüklendiğinde fotoğrafı çek
+document.addEventListener('DOMContentLoaded', fetchRecipePhoto);
+
 
 // Sayfa yüklendiğinde fotoğrafı çek
 document.addEventListener('DOMContentLoaded', fetchRecipePhoto);
+
+document.addEventListener('DOMContentLoaded', function() {
+  const recipeButton = document.querySelector('.recipe-button');
+  
+  recipeButton.addEventListener('click', function() {
+      // Butona tıklandığında metin rengi siyah olacak
+      recipeButton.style.color = '#000';
+  });
+});
