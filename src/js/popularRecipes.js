9b3aa4c68941688f16d4c6a775c3ea1e75f07c34
Mete <metlandus@hotmail.com.tr>
2024-10-11 21:51:06 +0300

    page all around optimized

--- src/js/popularRecipes.js
+++ src/js/popularRecipes.js
@@ -6,25 +6,25 @@ async function fetchData() {
     try {
         const response = await fetch("https://tasty-treats-backend.p.goit.global/api/recipes/popular");
         const data = await response.json();
-        console.log(data); 
+        console.log(data);
 
-        const recipes = data; 
+        const recipes = data;
         console.log(recipes);
 
         const popularRecipes = document.querySelector("#popular-recipes-gallery");
-        popularRecipes.innerHTML = ''; 
+        popularRecipes.innerHTML = '';
 
-     
+
         const fragment = document.createDocumentFragment();
 
         recipes.forEach((recipe) => {
             const li = document.createElement("li");
 
-         
+
             const recipeContainer = document.createElement("div");
             recipeContainer.classList.add("recipe-container");
 
-         
+
             const image = document.createElement("img");
             image.src = recipe.preview || 'default-image.jpg';
             image.alt = recipe.title || 'Unknown Recipe';
@@ -32,19 +32,19 @@ async function fetchData() {
             const contentDiv = document.createElement("div");
             contentDiv.classList.add("recipe-content");
 
-     
-            const title = document.createElement("h3");
+
+            const title = document.createElement("h2");
             title.textContent = recipe.title || 'Unknown Recipe';
             title.classList.add("recipe-content-title");
-            
-          
 
-          
+
+
+
             const instructions = document.createElement("p");
             instructions.textContent = recipe.description || 'No instructions provided';
             instructions.classList.add("recipe-content-instructions");
 
-         
+
             contentDiv.appendChild(title);
             contentDiv.appendChild(instructions);
             recipeContainer.appendChild(image);
@@ -54,11 +54,11 @@ async function fetchData() {
 
             console.log("Appending LI:", li);
 
-         
+
             fragment.appendChild(li);
         });
 
-      
+
         popularRecipes.appendChild(fragment);
 
     } catch (error) {
