40581ec9f82e1830f455bb6a13918d48815f6669
SubatYucel <subat.yucel1@gmail.com>
2024-10-11 11:34:53 +0300

    cards-tablet and desktop css done - bugfix on some css

--- src/js/cards.js
+++ src/js/cards.js
@@ -1,7 +1,11 @@
+const cardsList = document.querySelector('.cards-list');
+
 export async function fetchRecipes() {
   try {
     // API'den tarif verilerini çekiyoruz
-    const response = await fetch('https://tasty-treats-backend.p.goit.global/api/recipes?limit=9');
+    const response = await fetch(
+      'https://tasty-treats-backend.p.goit.global/api/recipes?limit=9'
+    );
     const data = await response.json();
 
     // Tarifleri gösterme fonksiyonunu çağır
@@ -10,47 +14,49 @@ export async function fetchRecipes() {
     console.error('API verisi alınırken hata oluştu:', error);
   }
 }
-const cardsList = document.querySelector('.cards-list');
+
 export function displayRecipes(recipes) {
-
-
   // Mevcut kartları temizleme (eğer gerekirse)
   cardsList.innerHTML = '';
 
   recipes.forEach(recipe => {
     // Rating için yıldızları oluşturma
-    const filledStars = Math.round(recipe.rating); // Kaç yıldız dolu olacağını hesapla
-    const emptyStars = 5 - filledStars; // Kaç yıldız boş kalacak
+    const filledStars = Math.round(recipe.rating);
+    const emptyStars = 5 - filledStars;
 
-    // Her kart için template literal oluşturuluyor
     const cardHTML = `
-      <li class="cards-listing" style="background-image: url(${recipe.preview});">
-        <div class="card-filter"></div>
-        <button class="heard-button"><svg class="svg-reset" width="22px" height="22px">
-          <use
-            class="heard-svg"
-            href="./svg/sprite.svg#icon-heart"
-          ></use>
+      <li class="cards-listing" style="background-image: url(${
+        recipe.preview
+      });">
+      <button class="heard-button" data-id="${recipe._id}">
+         <svg class="svg-heard" width="22px" height="22px">
+            <use id="heard-use" href="./svg/sprite.svg#icon-heart"></use>
         </svg></button>
-        <div class="text-container">${recipe.title}</div>
-        <div class="specification-text">${recipe.description}</div>
-        <div class="card-rating-container">
-          <div class="rating-container">
-            <button class="recipe-button">See recipe</button>
-            <div class="rating-star">
-            <p class="rating-text">${recipe.rating.toFixed(1)}</p>
-              ${`<svg class="svg-reset" width="18px" height="18px">
-          <use
-            href="./svg/sprite.svg#icon-star  "
-          ></use>
-        </svg>`.repeat(filledStars)}${`<svg class="svg-reset" width="18px" height="18px">
-          <use
-            href="./svg/sprite.svg#icon-emptystar"
-          ></use>
-        </svg>`.repeat(emptyStars)}
-            </div>
+
+      <div class="card-content-container">
+         <div class="text-container">
+            <h3 class="card-title">${recipe.title}</h3>
+              <p class="specification-text">${recipe.description}</p>
           </div>
-        </div>
+
+          <div class="card-rating-container">
+              <div class="rating-container">
+                  <p class="rating-text">${recipe.rating.toFixed(1)}</p>
+                  <div class="star-container">
+                    ${`<svg class="card-star-svg">
+                    <use href="./svg/sprite.svg#icon-star"></use>
+                      </svg>`.repeat(filledStars)}
+
+                    ${`<svg class="card-star-svg">
+                    <use href="./svg/sprite.svg#icon-emptystar"></use>
+                    </svg>`.repeat(emptyStars)}
+                  </div>    
+                </div>
+              
+            <button class="recipe-button" data-id="${
+              recipe._id
+            }">See recipe</button>
+        </div>      
       </li>
     `;
 
@@ -61,7 +67,7 @@ export function displayRecipes(recipes) {
   // Tüm recipe-button öğelerine tıklama olayı ekleyelim
   const recipeButtons = document.querySelectorAll('.recipe-button');
   recipeButtons.forEach(button => {
-    button.addEventListener('click', function() {
+    button.addEventListener('click', function () {
       // Butona tıklandığında metin rengi siyah olacak
       button.style.color = '#000';
     });
@@ -74,4 +80,3 @@ document.addEventListener('DOMContentLoaded', _ => {
     fetchRecipes();
   }, 100);
 });
-
