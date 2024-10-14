ce730c132438967faed99118fc75bb2323d5b0e3
pınar <pinar.dogans96@gmail.com>
2024-10-14 16:04:18 +0300

    modal added in popularRepices

--- src/js/header/orderPopup.js
+++ src/js/header/orderPopup.js
@@ -1,4 +1,8 @@
 const orderForm = document.getElementById('orderForm');
+const order = document.querySelector('.order');
+const closeButton = document.querySelector('.close-button');
+order.addEventListener('click',_=> openPopup());
+closeButton.addEventListener('click',_=> closePopup());
 function openPopup() {
     document.getElementById('popupOverlay').style.display = 'block';
     orderForm.reset();
