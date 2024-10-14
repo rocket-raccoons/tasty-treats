fa802c5e4bf55ba60bcfa29dbe7a15717900737c
subatyucel <subat.yucel1@gmail.com>
2024-10-14 13:32:15 +0000

    Deploying to gh-pages from @ rocket-raccoons/tasty-treats@50c509ae51fe3ed4d7939f151a3f3d563f31cdf3 🚀

--- favorites.js
+++ favorites.js
@@ -1,4 +1,4 @@
-import"./assets/footer-mascot-T9mK7Qjj.js";const n="/tasty-treats/assets/sprite-B_ptjhEe.svg",i=document.getElementById("message-container");function d(){const a=localStorage.getItem("favArr");let e=[];try{e=a?JSON.parse(a):[]}catch(r){console.error("Error parsing favArr from localStorage:",r),e=[]}const t=document.querySelector(".favoriteCards");return t&&(t.innerHTML=""),e.length===0&&(i.innerHTML="",i.innerHTML=`
+import"./assets/footer-mascot-T9mK7Qjj.js";const n="../img/svg/sprite.svg",i=document.getElementById("message-container");function d(){const r=localStorage.getItem("favArr");let e=[];try{e=r?JSON.parse(r):[]}catch(a){console.error("Error parsing favArr from localStorage:",a),e=[]}const t=document.querySelector(".favoriteCards");return t&&(t.innerHTML=""),e.length===0&&(i.innerHTML="",i.innerHTML=`
         <div class="fav-message-container">
 
         <img  src="./img/raccoon-sad-fav.png" class="fav-raccoon">
@@ -8,7 +8,7 @@ import"./assets/footer-mascot-T9mK7Qjj.js";const n="/tasty-treats/assets/sprite-
 
         <img  src="./img/raccoon-sad-fav.png" class="fav-chefs-hat">
         </div>
-        `),e}async function l(a){const e=await fetch(`https://tasty-treats-backend.p.goit.global/api/recipes/${a}`);if(!e.ok)throw new Error("Network response was not ok");return await e.json()}async function f(){const a=d();try{return await Promise.all(a.map(t=>l(t)))}catch(e){return console.error("Error fetching favorite recipes:",e),[]}}async function o(){const a=await f();if(a.length!==0){const e=document.querySelector(".favorite-cards");e.innerHTML="",a.forEach(t=>{const r=Math.round(t.rating),s=5-r,c=`
+        `),e}async function l(r){const e=await fetch(`https://tasty-treats-backend.p.goit.global/api/recipes/${r}`);if(!e.ok)throw new Error("Network response was not ok");return await e.json()}async function f(){const r=d();try{return await Promise.all(r.map(t=>l(t)))}catch(e){return console.error("Error fetching favorite recipes:",e),[]}}async function o(){const r=await f();if(r.length!==0){const e=document.querySelector(".favorite-cards");e.innerHTML="",r.forEach(t=>{const a=Math.round(t.rating),s=5-a,c=`
       <li class="cards-listing" style="background-image: url(${t.preview});">
         <button class="heard-button" data-id="${t._id}" aria-label="like-btn"><svg class="svg-heard add-to-fav" data-id="${t._id}" width="22px" height="22px"><use href="${n}#icon-heart-filled"></use></svg></button>
       <div class="card-content-container">
@@ -23,7 +23,7 @@ import"./assets/footer-mascot-T9mK7Qjj.js";const n="/tasty-treats/assets/sprite-
                   <div class="star-container">
                     ${`<svg class="card-star-svg">
                     <use href="${n}#icon-star"></use>
-                      </svg>`.repeat(r)}
+                      </svg>`.repeat(a)}
 
                     ${`<svg class="card-star-svg">
                     <use href="${n}#icon-emptystar"></use>
@@ -34,5 +34,5 @@ import"./assets/footer-mascot-T9mK7Qjj.js";const n="/tasty-treats/assets/sprite-
             <button class="recipe-button" data-id="${t._id}">See recipe</button>
         </div>      
       </li>
-    `;e.innerHTML+=c}),v()}}function v(){document.querySelectorAll(".heard-button").forEach(e=>{e.addEventListener("click",function(){const t=e.dataset.id,r=localStorage.getItem("favArr");if(r.includes(t)){let s=JSON.parse(r);s.splice(s.indexOf(t),1),localStorage.setItem("favArr",JSON.stringify(s))}o()})})}document.addEventListener("DOMContentLoaded",o);
+    `;e.innerHTML+=c}),v()}}function v(){document.querySelectorAll(".heard-button").forEach(e=>{e.addEventListener("click",function(){const t=e.dataset.id,a=localStorage.getItem("favArr");if(a.includes(t)){let s=JSON.parse(a);s.splice(s.indexOf(t),1),localStorage.setItem("favArr",JSON.stringify(s))}o()})})}document.addEventListener("DOMContentLoaded",o);
 //# sourceMappingURL=favorites.js.map
