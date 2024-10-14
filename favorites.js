5af9ce976e7f7b9fc56a5b66fc2bc5e843adf655
subatyucel <subat.yucel1@gmail.com>
2024-10-14 21:05:39 +0000

    Deploying to gh-pages from @ rocket-raccoons/tasty-treats@4e05e6da38f595fd4594ede9c2021c59273a05c7 🚀

--- favorites.js
+++ favorites.js
@@ -1,4 +1,4 @@
-import"./assets/footer-mascot-T9mK7Qjj.js";const n="assets/sprite-B_ptjhEe.svg",i=document.getElementById("message-container");function d(){const r=localStorage.getItem("favArr");let e=[];try{e=r?JSON.parse(r):[]}catch(a){console.error("Error parsing favArr from localStorage:",a),e=[]}const t=document.querySelector(".favoriteCards");return t&&(t.innerHTML=""),e.length===0&&(i.innerHTML="",i.innerHTML=`
+import"./assets/footer-mascot-3xfajzre.js";const n=document.getElementById("message-container");function c(){const r=localStorage.getItem("favArr");let e=[];try{e=r?JSON.parse(r):[]}catch(a){console.error("Error parsing favArr from localStorage:",a),e=[]}const t=document.querySelector(".favoriteCards");return t&&(t.innerHTML=""),e.length===0&&(n.innerHTML="",n.innerHTML=`
         <div class="fav-message-container">
 
         <img  src="./img/raccoon-sad-fav.png" class="fav-raccoon">
@@ -8,9 +8,9 @@ import"./assets/footer-mascot-T9mK7Qjj.js";const n="assets/sprite-B_ptjhEe.svg",
 
         <img  src="./img/raccoon-sad-fav.png" class="fav-chefs-hat">
         </div>
-        `),e}async function l(r){const e=await fetch(`https://tasty-treats-backend.p.goit.global/api/recipes/${r}`);if(!e.ok)throw new Error("Network response was not ok");return await e.json()}async function f(){const r=d();try{return await Promise.all(r.map(t=>l(t)))}catch(e){return console.error("Error fetching favorite recipes:",e),[]}}async function o(){const r=await f();if(r.length!==0){const e=document.querySelector(".favorite-cards");e.innerHTML="",r.forEach(t=>{const a=Math.round(t.rating),s=5-a,c=`
+        `),e}async function d(r){const e=await fetch(`https://tasty-treats-backend.p.goit.global/api/recipes/${r}`);if(!e.ok)throw new Error("Network response was not ok");return await e.json()}async function l(){const r=c();try{return await Promise.all(r.map(t=>d(t)))}catch(e){return console.error("Error fetching favorite recipes:",e),[]}}async function i(){const r=await l();if(r.length!==0){const e=document.querySelector(".favorite-cards");e.innerHTML="",r.forEach(t=>{const a=Math.round(t.rating),s=5-a,o=`
       <li class="cards-listing" style="background-image: url(${t.preview});">
-        <button class="heard-button" data-id="${t._id}" aria-label="like-btn"><svg class="svg-heard add-to-fav" data-id="${t._id}" width="22px" height="22px"><use href="${n}#icon-heart-filled"></use></svg></button>
+        <button class="heard-button" data-id="${t._id}" aria-label="like-btn"><svg class="svg-heard add-to-fav" data-id="${t._id}" width="22px" height="22px"><use href="./svg/sprite.svg#icon-heart-filled"></use></svg></button>
       <div class="card-content-container">
          <div class="text-container">
             <h3 class="card-title">${t.title}</h3>
@@ -22,11 +22,11 @@ import"./assets/footer-mascot-T9mK7Qjj.js";const n="assets/sprite-B_ptjhEe.svg",
                   <p class="rating-text">${t.rating.toFixed(1)}</p>
                   <div class="star-container">
                     ${`<svg class="card-star-svg">
-                    <use href="${n}#icon-star"></use>
+                    <use href="./svg/sprite.svg#icon-star"></use>
                       </svg>`.repeat(a)}
 
                     ${`<svg class="card-star-svg">
-                    <use href="${n}#icon-emptystar"></use>
+                    <use href="./svg/sprite.svg#icon-emptystar"></use>
                     </svg>`.repeat(s)}
                   </div>    
                 </div>
@@ -34,5 +34,5 @@ import"./assets/footer-mascot-T9mK7Qjj.js";const n="assets/sprite-B_ptjhEe.svg",
             <button class="recipe-button" data-id="${t._id}">See recipe</button>
         </div>      
       </li>
-    `;e.innerHTML+=c}),v()}}function v(){document.querySelectorAll(".heard-button").forEach(e=>{e.addEventListener("click",function(){const t=e.dataset.id,a=localStorage.getItem("favArr");if(a.includes(t)){let s=JSON.parse(a);s.splice(s.indexOf(t),1),localStorage.setItem("favArr",JSON.stringify(s))}o()})})}document.addEventListener("DOMContentLoaded",o);
+    `;e.innerHTML+=o}),v()}}function v(){document.querySelectorAll(".heard-button").forEach(e=>{e.addEventListener("click",function(){const t=e.dataset.id,a=localStorage.getItem("favArr");if(a.includes(t)){let s=JSON.parse(a);s.splice(s.indexOf(t),1),localStorage.setItem("favArr",JSON.stringify(s))}i()})})}document.addEventListener("DOMContentLoaded",i);
 //# sourceMappingURL=favorites.js.map
