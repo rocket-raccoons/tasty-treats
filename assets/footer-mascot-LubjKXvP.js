bfc648d9835ad382415c370123b33e111c941724
subatyucel <subat.yucel1@gmail.com>
2024-10-14 07:36:59 +0000

    Deploying to gh-pages from @ rocket-raccoons/tasty-treats@2036f37b29f8b636ec41400c232089147ca7089d 🚀

---
+++ assets/footer-mascot-LubjKXvP.js
@@ -0,0 +1,17 @@
+(function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))s(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const i of t.addedNodes)i.tagName==="LINK"&&i.rel==="modulepreload"&&s(i)}).observe(document,{childList:!0,subtree:!0});function c(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function s(e){if(e.ep)return;e.ep=!0;const t=c(e);fetch(e.href,t)}})();document.addEventListener("DOMContentLoaded",()=>{const a=document.querySelector(".footer-list"),o=document.getElementById("myModal"),c=document.getElementById("openModalBtn"),s=document.getElementsByClassName("close")[0],e={name:"Mete CİVELEK",role:"Team Leader, Developer",imgSrc:"https://via.placeholder.com/50",github:"https://github.com/metlandus"},t={name:"Şubat YÜCEL",role:"Team Leader, Developer",imgSrc:"https://via.placeholder.com/50",github:"https://github.com/subatyucel"},i={name:"Deniz Sofia ULUTAŞ",role:"Scrum Master, Developer",imgSrc:"https://via.placeholder.com/50",github:"https://github.com/sofia11244"},m={name:"Onur AKARSU",role:"Developer",imgSrc:"https://via.placeholder.com/50",github:"https://github.com/RiveTroy"},u={name:"Pınar ÜNLÜ",role:"Developer",imgSrc:"https://via.placeholder.com/50",github:"https://github.com/pinarunlu"},p={name:"Mehmet ARDIÇ",role:"Developer",imgSrc:"https://via.placeholder.com/50",github:"https://github.com/ArdicMehmet"},d={name:"Murat ÇOLAK",role:"Developer",imgSrc:"https://via.placeholder.com/50",github:"https://github.com/zipkaniar"},h={name:"Mr. Raccoon",role:"Mascot",imgSrc:"https://via.placeholder.com/50",github:"https://github.com/rocket-racoons"},r=({name:n,role:g,imgSrc:f,github:b})=>{const l=document.createElement("li");return l.classList.add("footer-list-item"),l.innerHTML=`
+        <img class="footer-list-item-img" src="${f}" alt="">
+        
+        <div class="footer-list-item-info">    
+            <h2 class="footer-list-item-name">${n}</h2>
+            <p class="footer-list-item-text">${g}</p>
+            <div class="footer-list-item-links">
+            <a class="footer-list-item-link" href="${b}" target="_blank">
+                <svg class="icon-github-logo" width="20" height="20">
+                    <use xlink:href="./svg/github-icon.svg#icon-github-logo"></use>
+                </svg>
+                GitHub 
+            </a>
+            </div>
+        </div>
+         `,l};a.append(r(e),r(t),r(i),r(m),r(u),r(p),r(d),r(h)),c.onclick=function(){o.style.display="block"},s.onclick=function(){o.style.display="none"},window.onclick=function(n){n.target===o&&(o.style.display="none")}});
+//# sourceMappingURL=footer-mascot-LubjKXvP.js.map
--- favorites.js
+++ favorites.js
@@ -1,35 +1,38 @@
-import"./assets/styles-CUHzzICJ.js";function i(){const s=localStorage.getItem("favArr");let t=[];try{t=s?JSON.parse(s):[]}catch(r){console.error("Error parsing favArr from localStorage:",r),t=[]}const e=document.getElementById("message-container");return e&&(e.innerHTML=""),t.length===0&&e&&(e.innerHTML=`
+import"./assets/footer-mascot-LubjKXvP.js";const n=document.getElementById("message-container");function c(){const r=localStorage.getItem("favArr");let e=[];try{e=r?JSON.parse(r):[]}catch(a){console.error("Error parsing favArr from localStorage:",a),e=[]}const t=document.querySelector(".favoriteCards");return t&&(t.innerHTML=""),e.length===0&&(n.innerHTML="",n.innerHTML=`
         <div class="fav-message-container">
-        <svg class="fav-chefs-hat" type="fav-chefs-hat-svg" width="68" height="58">
-            <use href="./svg/sprite.svg#icon-chef-hat">
-        </svg> <br><br><br>
-    <p class="fav-message">It appears that you haven't added any recipes to your favorites yet. To get started, you can add recipes that you like to your favorites for easier access in the future.</p>
-        </div>
-        `),t}async function o(s){const t=await fetch(`https://tasty-treats-backend.p.goit.global/api/recipes/${s}`);if(!t.ok)throw new Error("Network response was not ok");return await t.json()}async function c(){const s=i();try{return await Promise.all(s.map(e=>o(e)))}catch(t){return console.error("Error fetching favorite recipes:",t),[]}}async function d(){const s=await c();if(s.length!==0){const t=document.getElementById("message-container");t.innerHTML="",s.forEach(e=>{const r=Math.round(e.rating),a=5-r,n=`
-      <li class="cards-listing" style="background-image: url(${e.preview});">
 
+        <img  src="./img/raccoon-sad-fav.png" class="fav-raccoon">
+        
+        <p class="fav-message">It appears that you haven't added any recipes to your favorites yet. To get started, you can add recipes that you like to your favorites for easier access in the future.</p>
+
+
+        <img  src="./img/raccoon-sad-fav.png" class="fav-chefs-hat">
+        </div>
+        `),e}async function d(r){const e=await fetch(`https://tasty-treats-backend.p.goit.global/api/recipes/${r}`);if(!e.ok)throw new Error("Network response was not ok");return await e.json()}async function l(){const r=c();try{return await Promise.all(r.map(t=>d(t)))}catch(e){return console.error("Error fetching favorite recipes:",e),[]}}async function i(){const r=await l();if(r.length!==0){const e=document.querySelector(".favorite-cards");e.innerHTML="",r.forEach(t=>{const a=Math.round(t.rating),s=5-a,o=`
+      <li class="cards-listing" style="background-image: url(${t.preview});">
+        <button class="heard-button" data-id="${t._id}" aria-label="like-btn"><svg class="svg-heard add-to-fav" data-id="${t._id}" width="22px" height="22px"><use href="./svg/sprite.svg#icon-heart-filled"></use></svg></button>
       <div class="card-content-container">
          <div class="text-container">
-            <h3 class="card-title">${e.title}</h3>
-              <p class="specification-text">${e.description}</p>
+            <h3 class="card-title">${t.title}</h3>
+              <p class="specification-text">${t.description}</p>
           </div>
 
           <div class="card-rating-container">
               <div class="rating-container">
-                  <p class="rating-text">${e.rating.toFixed(1)}</p>
+                  <p class="rating-text">${t.rating.toFixed(1)}</p>
                   <div class="star-container">
                     ${`<svg class="card-star-svg">
                     <use href="./svg/sprite.svg#icon-star"></use>
-                      </svg>`.repeat(r)}
+                      </svg>`.repeat(a)}
 
                     ${`<svg class="card-star-svg">
                     <use href="./svg/sprite.svg#icon-emptystar"></use>
-                    </svg>`.repeat(a)}
+                    </svg>`.repeat(s)}
                   </div>    
                 </div>
               
-            <button class="recipe-button" data-id="${e._id}">See recipe</button>
+            <button class="recipe-button" data-id="${t._id}">See recipe</button>
         </div>      
       </li>
-    `;t.innerHTML+=n})}}document.addEventListener("DOMContentLoaded",d);
+    `;e.innerHTML+=o}),v()}}function v(){document.querySelectorAll(".heard-button").forEach(e=>{e.addEventListener("click",function(){const t=e.dataset.id,a=localStorage.getItem("favArr");if(a.includes(t)){let s=JSON.parse(a);s.splice(s.indexOf(t),1),localStorage.setItem("favArr",JSON.stringify(s))}i()})})}document.addEventListener("DOMContentLoaded",i);
 //# sourceMappingURL=favorites.js.map
